37f734d5a60a648ea5c45b5d669f633f1fd7985b
Efim Sivolap <efim.sivolap@gmail.com>
2022-10-22 17:53:13 +0300

    login_page

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:1078319116100:web:83b3231f093b008802f111";
+  static String apiKey = "AIzaSyACuLESEGvZ36XaqxUzbnwt2WJG6lZ-0k8";
+  static String messagingSenderId = "1078319116100";
+  static String projectId = "phonelist-ccc02";
+  final primaryColor = const Color(0xFFee7b64);
+}
