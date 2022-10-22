6d8c78def1c28a1ebc306fb331943dac6edc265f
Efim Sivolap <efim.sivolap@gmail.com>
2022-10-22 18:09:25 +0300

    register_page

--- lib/pages/auth/register_page.dart
+++ lib/pages/auth/register_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:phone_book_app/pages/auth/login_page.dart';
 
 import '../../widgets/widgets.dart';
 
@@ -35,7 +36,34 @@ class _RegisterPageState extends State<RegisterPage> {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                Image.asset("assets/login.png"),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text("Create your account now"),
+                Image.asset("assets/register.png"),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(
+                      labelText: "Full Name",
+                      prefixIcon: Icon(
+                        Icons.person,
+                        color: Theme.of(context).primaryColor,
+                      )),
+                  onChanged: (val) {
+                    setState(() {
+                      fullName = val;
+                    });
+                  },
+                  validator: (val) {
+                    if (val!.isNotEmpty) {
+                      return null;
+                    } else {
+                      return "Name can't be empty";
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
                 TextFormField(
                   decoration: textInputDecoration.copyWith(
                       labelText: "Email",
@@ -46,7 +74,6 @@ class _RegisterPageState extends State<RegisterPage> {
                   onChanged: (val) {
                     setState(() {
                       email = val;
-                      print(email);
                     });
                   },
                   validator: (val) {
@@ -94,7 +121,7 @@ class _RegisterPageState extends State<RegisterPage> {
                       ),
                     ),
                     child: const Text(
-                      "Sign In",
+                      "Register",
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 16,
@@ -110,20 +137,20 @@ class _RegisterPageState extends State<RegisterPage> {
                 ),
                 Text.rich(
                   TextSpan(
-                    text: "Don't have an account?",
+                    text: "Already have an account?",
                     style: TextStyle(
                       color: Colors.black,
                       fontSize: 14,
                     ),
                     children: <TextSpan>[
                       TextSpan(
-                          text: "Register here",
+                          text: "Login now",
                           style: const TextStyle(
                               color: Colors.black,
                               decoration: TextDecoration.underline),
                           recognizer: TapGestureRecognizer()
                             ..onTap = () {
-                              nextScreen(context, const RegisterPage());
+                              nextScreen(context, const LoginPage());
                             }),
                     ],
                   ),
