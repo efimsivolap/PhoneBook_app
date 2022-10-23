b99d5c9df70ea1eb7646f1b2f0b4e13b835df2f9
Efim Sivolap <efim.sivolap@gmail.com>
2022-10-23 11:04:26 +0300

    firebase_auth

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:phone_book_app/pages/search_page.dart';
+import 'package:phone_book_app/service/auth_service.dart';
+import 'package:phone_book_app/widgets/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +11,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  AuthService authService = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("HomePAge"),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                nextScreen(context, SearchPage());
+              },
+              icon: const Icon(Icons.search))
+        ],
+        elevation: 0,
+        backgroundColor: Theme.of(context).primaryColor,
+        centerTitle: true,
+        title: Text(
+          "Contacts",
+          style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 50),
+          children: <Widget>[
+            Icon(
+              Icons.account_circle,
+              size: 150,
+              color: Colors.grey,
+            )
+          ],
+        ),
       ),
     );
   }
